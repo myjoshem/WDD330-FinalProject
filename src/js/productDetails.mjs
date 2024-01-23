bbf8e3a2e3b6d7f9351333ef3b79fc7080e14c65
gavin-cannon <gavincannon@byui.edu>
2024-01-23 13:24:17 -0700

    adding my code to main. partial team 3 commit to main.

---
+++ src/js/productDetails.mjs
@@ -0,0 +1,12 @@
+import { addToCart } from "./product.js";
+
+let storage;
+
+export function productDetails(productId) {}
+
+function renderProductDetails() {}
+
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
