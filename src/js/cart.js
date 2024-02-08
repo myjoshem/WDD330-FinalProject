0e434b97fc520673499d1ad30a398d44685ba300
gavin-cannon <gavincannon@byui.edu>
2024-02-08 12:43:37 -0700

    added some console logs and adjust cart local storage call

--- src/js/cart.js
+++ src/js/cart.js
@@ -48,18 +48,18 @@ renderCartContents();
 // for incrementing or decrementing quantities already in the cart
 // Function to handle incrementing the quantity
 function incrementQuantity(id) {
-  /* 
-  const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  const cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
   const itemIndex = cartItems.findIndex((item) => item.id === id);
+  console.log(itemIndex);
   if (itemIndex !== -1) {
     cartItems[itemIndex].quantity++;
     localStorage.setItem("cart", JSON.stringify(cartItems));
-  } */
+  }
 }
 
 // Function to handle decrementing the quantity
 function decrementQuantity(id) {
-  const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+  const cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
   const itemIndex = cartItems.findIndex((item) => item.id === id);
   if (itemIndex !== -1 && cartItems[itemIndex].quantity > 1) {
     cartItems[itemIndex].quantity--;
@@ -73,6 +73,7 @@ setClick("click", (e) => {
   if (target.classList.contains("increment")) {
     const id = target.dataset.id;
     incrementQuantity(id);
+    console.log("increment");
     // Update UI or perform any other actions after incrementing
     renderCartContents();
   } else if (target.classList.contains("decrement")) {
