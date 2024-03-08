0e65bfc8b7e96553cac077dce39274307f9f19b7
Gavin Cannon <104046105+gavin-cannon@users.noreply.github.com>
2024-03-08 16:36:32 -0700

    Added sort functionality

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -3,12 +3,110 @@ import { setCartSuperscriptHTML } from "./productDetails.mjs";
 import { getParam, loadHeaderFooter, qs } from "./utils.mjs";
 
 let category = getParam("category");
-
+let button = qs("#sort__button");
 
 setUpProductListPage();
 async function setUpProductListPage() {
   loadHeaderFooter();
   await productList(".product-list", category);
-  setCartSuperscriptHTML()
+  setCartSuperscriptHTML();
   modalListenerSetUp();
+}
+button.addEventListener("click", sortList);
+
+function sortList() {
+  console.log("Hello");
+  let productListElement = qs(".product-list");
+  //let productsArray = [];
+  let productsArray = Array.from(
+    productListElement.querySelectorAll(".product-card"),
+  );
+
+  //for (var i = 0; i < products.length; i++) {
+  //  productsArray.push(products[i]);
+  //}
+  console.log(productsArray);
+  let sortType = qs("#sort__type").value;
+  let sortDirection = qs("#sort__direction").value;
+  console.log(sortType);
+
+  if (sortType == "name") {
+    if (sortDirection == "asc") {
+      productsArray.sort(function (a, b) {
+        var productA = a
+          .querySelector(".card__name")
+          .textContent.trim()
+          .toLowerCase();
+        var productB = b
+          .querySelector(".card__name")
+          .textContent.trim()
+          .toLowerCase();
+        if (productA < productB) return 1;
+        if (productA > productB) return -1;
+        return 0;
+      });
+      productListElement.innerHTML = "";
+      productsArray.forEach(function (product) {
+        productListElement.appendChild(product);
+      });
+    } else {
+      productsArray.sort(function (a, b) {
+        var productA = a
+          .querySelector(".card__name")
+          .textContent.trim()
+          .toLowerCase();
+        var productB = b
+          .querySelector(".card__name")
+          .textContent.trim()
+          .toLowerCase();
+        if (productA < productB) return -1;
+        if (productA > productB) return 1;
+        return 0;
+      });
+      productListElement.innerHTML = "";
+      productsArray.forEach(function (product) {
+        productListElement.appendChild(product);
+      });
+    }
+  }
+  if (sortType == "price") {
+    console.log("inside price");
+    if (sortDirection == "asc") {
+      productsArray.sort(function (a, b) {
+        var productA = Number(
+          a.querySelector(".product-card__price").innerHTML,
+        );
+        var productB = Number(
+          b.querySelector(".product-card__price").innerHTML,
+        );
+        console.log(productA, productB);
+        if (productA < productB) return -1;
+        if (productA > productB) return 1;
+        return 0;
+      });
+      productListElement.innerHTML = "";
+      productsArray.forEach(function (product) {
+        console.log(product);
+        productListElement.appendChild(product);
+      });
+    } else {
+      productsArray.sort(function (a, b) {
+        var productA = Number(
+          a.querySelector(".product-card__price").innerHTML,
+        );
+        var productB = Number(
+          b.querySelector(".product-card__price").innerHTML,
+        );
+        console.log(productA, productB);
+        if (productA < productB) return 1;
+        if (productA > productB) return -1;
+        return 0;
+      });
+      productListElement.innerHTML = "";
+      productsArray.forEach(function (product) {
+        console.log(product);
+        productListElement.appendChild(product);
+      });
+    }
+  }
 }
